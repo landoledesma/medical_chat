54e75ec56d371e51d3bd12ad208c40e3339c521a
Lando Ledesma <lando_ledesma@outlook.com>
2023-09-29 22:57:26 -0600

    primer commit del proyecto

---
+++ ingest.py
@@ -0,0 +1,26 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import PyPDFLoader,DirectoryLoader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from dotenv import load_dotenv
+import os
+
+load_dotenv("token.env")
+
+
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+DATA_PATH = "data/"
+DB_FAISS_PATH = "vectorstore/db_faiss"
+
+
+def create_vectordb():
+    embeddings = OpenAIEmbeddings()
+    loader = DirectoryLoader(DATA_PATH,glob='*.pdf',loader_cls=PyPDFLoader)
+    documents = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=100,
+        chunk_overlap = 100
+    )
+    texts = text_splitter.split_documents(documents)
+
+    db = FAISS.from_documents(texts,embeddings)
