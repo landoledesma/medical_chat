8a9de76fe93810f6e36e7c4ae526550ce1a659d0
Lando Ledesma <lando_ledesma@outlook.com>
2023-10-03 14:35:54 -0600

    definiendo las funciones de uso para la aplicacion

--- model.py
+++ model.py
@@ -5,7 +5,6 @@ from langchain.chains import RetrievalQA,ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
 import chainlit as cl
 
-
 DB_FAISS_PATH = "vectorstore/db_faiss"
 
 template_bot = """ 
@@ -29,3 +28,26 @@ def load_llm():
         max_tokens=250,temperature=0.5
         )
     return llm
+
+def retrival_qa_chain(llm,prompt,db):
+    qa_chain = RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type="stuff",
+        retriever = db.as_retriever(search_kwargs={'k':2}),
+        return_source_documents = True,
+        chain_type_kwargs = {'prompt':prompt}
+    )
+    return qa_chain
+
+def qa_bot():
+    embeddings = OpenAIEmbeddings()
+    db = FAISS.load_local(DB_FAISS_PATH,embeddings)
+    llm = load_llm
+    qa_prompt = retrival_qa_chain(llm,qa_prompt,db)
+
+    return qa_bot
+
+def final_result(query):
+    qa_result = qa_bot
+    response = qa_result({'query':query})
+    return response
